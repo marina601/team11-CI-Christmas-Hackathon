b22afd47979166903636d75b7c850b7b244375aa
marina601 <marina601@live.com>
2021-12-10 11:55:59 +0000

    Initial set up

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from flask import (
+    Flask, flash, render_template, redirect,
+    request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sslify import SSLify
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+sslify = SSLify(app)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    """
+    Find Products from Mongo db collection
+    """
+    wishes = mongo.db.products.find()
+
+    return render_template("index.html", wishes=wishes)
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
