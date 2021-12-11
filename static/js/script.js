211b5287ade0bc37df6ff67b89afd84e9b2b4a1a
Anthony O'Brien <aux.fuse@yahoo.com>
2021-12-11 13:08:14 +0000

    basic navbar in place with default styles and tested with user session and responsiveness

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,11 @@
 console.log("test");
 
+// Navbar toggler initialisation
+document.addEventListener('DOMContentLoaded', () => {
+    let elems = document.querySelectorAll('.sidenav');
+    let instances = M.Sidenav.init(elems);
+});
+
 // Initialize the select element 
 
 $(document).ready(function(){
